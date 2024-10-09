121bb533ebcbcd57628f17b44aa5c7614a4c3cfb
haco8534 <siduki0107@yahoo.co.jp>
2024-10-09 09:23:10 +0900

    a

---
+++ src/component/Search-content.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchContent= () => {
+    return(
+        <div>
+            <input value="テキストを入力"></input>
+        </div>
+    )
+}
+
+export default SearchContent;
--- src/component/header.js
+++ src/component/header.js
@@ -1,14 +1,17 @@
-import HeaderSearch from "./header.search.js";
 import HeaderCart from "./header.cart.js";
 import HeaderLogin from "./header.login.js";
 import style from "../styles/header.module.css"
 
 const Header = () => {
     return (
-        <div className = {style.header}>
-            <HeaderSearch></HeaderSearch> 
-            <HeaderCart></HeaderCart>
-            <HeaderLogin></HeaderLogin>                
+        <div className={style.app}>
+            <div className = {style.header}>
+                <HeaderCart></HeaderCart>
+                <HeaderLogin></HeaderLogin> 
+            </div>
+            <div className={style.header}>
+            
+            </div> 
         </div>
     );
 }
