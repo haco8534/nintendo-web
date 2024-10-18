095f6ae9f8b61ecae5e4cdc6891a2a033f1cd272
haco8534 <siduki0107@yahoo.co.jp>
2024-10-18 19:25:15 +0900

    merged pickup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import Topics from './component/topics/topics';
-import PickUp from './component/pickup/pickup';
 
 function App() {
   return (
     <div className = "App">
-      <Topics></Topics>
-      <PickUp></PickUp>
     </div>
   );
 }
