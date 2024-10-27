1027f4ce77126d425d762e8fd6aa78592d1c3b7e
haco8534 <siduki0107@yahoo.co.jp>
2024-10-27 21:03:12 +0900

    a

--- src/component/device/list.js
+++ src/component/device/list.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Software from './softwareList';
 import Smartphone from './smartphonList';
+import Schedule from './schedule';
 import style from '../../styles/device/list.module.css';
 
-const List = () => {
+const Device = () => {
     return (
         <div className={style.list}>
             <Software></Software>
             <div className={style.divider}></div>
             <Smartphone></Smartphone>
+            <Schedule></Schedule>
         </div>
     );
 }
 
-export default List;
+export default Device;
